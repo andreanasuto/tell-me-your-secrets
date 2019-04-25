5ee1b66c0b6a2bd2161659d86e29cf55ca7262d9
andreanasuto <andrea.nasuto@gmail.com>
2019-04-25 12:42:28 -0400

    nested resources update

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,9 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :secrets
-<<<<<<< HEAD
     # POST /users/:id/secrets - a secret belongs to a user!
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
-=======
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  end
-
-  # POST /users/:id/secrets - a secret belongs to a user!
->>>>>>> 9a783669ddcb2baf2ff18a0e5c14490489ba43ba
 
 end
