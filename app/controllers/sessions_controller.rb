1e7d115cc395936d8f695327810044f29bc204c1
andreanasuto <andrea.nasuto@gmail.com>
2019-04-29 18:55:18 -0400

    create action for sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user_secrets_path(user)
+    else
+      #create a error message. let the user know what's wrong.
+      redirect_to login_path
+    end
+
   end
 
   def destroy
+    session[:user_id].destroy
   end
+
 end
