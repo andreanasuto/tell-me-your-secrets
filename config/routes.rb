675de497ddf48aa0e4dd5eda6abd4c8b62fb2ef1
andreanasuto <andrea.nasuto@gmail.com>
2019-04-29 18:19:45 -0400

    /login, /logout, /destroy routes and actions created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # get '/patients/:id', to: 'patients#show', as: 'patient'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
 
   resources :users do
     resources :secrets
