8e7e6293309d981410224e9099a9a849533a859b
andreanasuto <andrea.nasuto@gmail.com>
2019-04-30 19:22:15 -0400

    create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_token :auth_token
-  has_secure_password
+  #has_secure_token :auth_token
+  #has_secure_password
   has_many :secrets
+  has_many :locations, through: :secrets
 end
