1628c2532bb0a1d5a8fde0bc3b1e6e2dc11d27d0
andreanasuto <andrea.nasuto@gmail.com>
2019-04-30 13:49:51 -0400

    created some defaults Secretes in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+secrets = Secret.create([
+  { sentence: 'I love Adam Curtis!', latitude: 40.703112, longitude: -73.985296},
+  {sentence: 'I genuinely think Luis Nazario de Lima Ronaldo is an artist', latitude: 40.711636, longitude: -73.993349}
+  ])
