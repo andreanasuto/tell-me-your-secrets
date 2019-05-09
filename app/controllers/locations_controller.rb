91d4bcc3313c77891389e0719fafb5aa060dd0a3
andreanasuto <andrea.nasuto@gmail.com>
2019-05-09 17:07:42 -0400

    generate a controller for Locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,9 @@
 class LocationsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def index
+    @locations = Location.all
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 end
