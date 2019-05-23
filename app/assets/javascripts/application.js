9ca8e400f954acca951fb078c40ac3277fb760f2
andreanasuto <andrea.nasuto@gmail.com>
2019-05-23 18:55:03 -0400

    moved the js functions to getLocation to the right js folder in asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,34 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+function geolocationSuccess(position) {
+  // check if the location input value are empty and fill them with if so.
+  if (document.getElementById('secret_location_latitude').value == "") {
+  document.getElementById('secret_location_latitude').value = position.coords.latitude;
+  document.getElementById('secret_location_longitude').value = position.coords.longitude;
+  }
+};
+
+function geolocationError(error) {
+  // it handles errors
+  x = document.getElementById('location_errors')
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      x.style.border = "1px solid red"
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      x.style.border = "1px solid red"
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      x.style.border = "1px solid red"
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      x.style.border = "1px solid red"
+      break;
+    }
+};
