2d2bc760dea91f9468c4ee5cb708a01325022df1
andreanasuto <andrea.nasuto@gmail.com>
2019-05-23 19:05:45 -0400

    it validates the presence of a location for each secret, it defines #close_secret method, it defines a #check_user method, commented previous methods used to return a secret

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -3,33 +3,47 @@ class Secret < ApplicationRecord
   belongs_to :location
 
   validates :sentence, presence: true
+  validates_associated :location
+  validates_presence_of :location
 
-def secrets_close_to_a_location
-  # this method should return secrets that are close to a location
+def close_secrets
+  secrets = []
+  self.location.nearbys.collect do |l|
+    l.secrets.each do |secret|
+      secrets << secret
+    end
+  end
+  secrets.reject { |e| e.user == self.user }
+end
+
+def check_user
+  if params[:id] || params[:user_id]
+    User.find(params[:id] || params[:user_id]) == current_user
+  end
 end
 
 # return a random secret once a secret is shared
-def returnSecret
-  secret_found = Secret.all.sample
-  # it avoids that it returns the secret just shared by itself
-    if secret_found == self
-       return self.returnSecret
-    else
-      return secret_found
-    end
-end
-
-# more complex method to return secret founds
-   def returnSecretComplex
-    @secret_found = self.findSecret
-    if @secret_found != self
-      return @secret_found
-    else
-        return self.findSecret.sample
-     end
-    end
-
-  def findSecret
-      Secret.all.find_all{ |secret| self.sentence.split.find { |i| secret.sentence.include?(i) } }
-  end
+# def returnSecret
+#   secret_found = Secret.all.sample
+#   # it avoids that it returns the secret just shared by itself
+#     if secret_found == self
+#        return self.returnSecret
+#     else
+#       return secret_found
+#     end
+# end
+#
+# # more complex method to return secret founds
+#    def returnSecretComplex
+#     @secret_found = self.findSecret
+#     if @secret_found != self
+#       return @secret_found
+#     else
+#         return self.findSecret.sample
+#      end
+#     end
+#
+#   def findSecret
+#       Secret.all.find_all{ |secret| self.sentence.split.find { |i| secret.sentence.include?(i) } }
+#   end
 end
