856386d9cfaf09acd361bbe8cd51efc4ba7c108f
andreanasuto <andrea.nasuto@gmail.com>
2019-05-23 19:02:03 -0400

    helpers method created to refactor Locations controller

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,5 +3,33 @@ module LocationsHelper
   #  location.secrets << Secret.new
   #  location
   #end
+  def locations_name
+    Location.all.collect { |l|
+      unless l.city.nil?
+        l.city
+      else
+        l.address
+      end
+    }.uniq
+  end
 
+  def find_location
+    Location.all.find_by(city: params[:id]) || Location.all.find_by(address: params[:id])
+  end
+
+  def check_near_locations
+    current_user.locations.near(params[:id]).empty?
+  end
+
+  def close_locations_near_to_a_slug
+    Location.all.near(params[:id])
+  end
+
+  def has_access_to_locations?
+    if @locations
+      render @locations
+    elsif @location
+      render partial: 'locations/city'
+    end
+  end
 end
