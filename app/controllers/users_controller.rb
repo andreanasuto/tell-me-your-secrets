913e6e051c57c41af9102522db65f07599144491
andreanasuto <andrea.nasuto@gmail.com>
2019-05-23 19:01:07 -0400

    Users controller refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,27 @@
 class UsersController < ApplicationController
+  before_action :authentification_required, only: [:show, :edit]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to new_secret_path
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to new_secret_path
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def edit
-  end
-
-  def delete
-  end
-
-  def update
-  end
-
   private
 
   def user_params
-    params.require(:user).permit(:name, :password_digest)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
