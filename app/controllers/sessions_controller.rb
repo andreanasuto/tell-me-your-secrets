cd7de47df23aaf5091e3d90e0c17956ae547f726
andreanasuto <andrea.nasuto@gmail.com>
2019-05-23 19:00:45 -0400

    session controller handles signup through a Google account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      redirect_to user_secrets_path(user)
+    if !auth_hash.nil?
+      user = User.find_or_create_from_auth_hash(auth_hash)
+      session[:user_id] = user.id
+      redirect_to new_secret_path
     else
-      #create a error message. let the user know what's wrong.
-      redirect_to login_path
+      user = User.find_by(email: params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        log_in(user)
+        redirect_to new_secret_path
+      else
+        #create a error message. let the user know what's wrong.
+        redirect_to login_path
+      end
     end
-
   end
 
   def destroy
-    session[:user_id].destroy
+    session.destroy
+    redirect_to login_path
+  end
+
+protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
 end
