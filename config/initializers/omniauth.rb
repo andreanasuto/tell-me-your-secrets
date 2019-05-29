383087e52dc2720bb94e56d49f8fdeebab14f701
andreanasuto <andrea.nasuto@gmail.com>
2019-05-29 17:37:08 -0400

    edits google_id key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :google_oauth2, ENV['GOOGLE_AUTH_KEY'], ENV['GOOGLE_AUTH_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_AUTH_SECRET']
 end
