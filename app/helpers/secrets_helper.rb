20bde275ed5dad2f43be4b12f92370a1fd033563
andreanasuto <andrea.nasuto@gmail.com>
2019-06-18 18:40:40 -0400

    defines helper that checks if the route is nested in a location (eg. /locations/:id/secrets) or not (eg. /secrets) and render information correctly

--- app/helpers/secrets_helper.rb
+++ app/helpers/secrets_helper.rb
@@ -5,4 +5,13 @@ module SecretsHelper
       render partial: 'form', locals: {secret: secret, location: location }
     end
   end
+
+  def are_secrets_nested?(secrets)
+    if params[:location_id]
+      location = Location.find(params[:location_id])
+      render partial: 'secret_location_nested', locals: {secrets: secrets, location: location}
+    else
+      render @secrets
+    end
+  end
 end
