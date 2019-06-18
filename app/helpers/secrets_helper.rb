df6788e514b33ad00b38a7fc5830c8b24a8c1e54
andreanasuto <andrea.nasuto@gmail.com>
2019-06-18 18:39:32 -0400

    defines helper that checks if a route is nested in user profile and render correct partial and local variables

--- app/helpers/secrets_helper.rb
+++ app/helpers/secrets_helper.rb
@@ -1,2 +1,8 @@
 module SecretsHelper
+
+  def insert_a_new_secret_option(secret, location)
+    if !params[:user_id]
+      render partial: 'form', locals: {secret: secret, location: location }
+    end
+  end
 end
