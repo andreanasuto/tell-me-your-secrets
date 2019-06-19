9ca3edb7e2dfe5df92aca4b1ecc32209e3704877
andreanasuto <andrea.nasuto@gmail.com>
2019-06-18 21:50:54 -0400

    adds json response logic to location controller at #show action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,6 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
     @locations_name = Location.locations_name
-
   end
 
   def northernmost
@@ -17,9 +16,14 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = Location.find_location_by_city_or_address(params) #find the location based on params[:location_id]
+    @location = Location.find_location_by_city_or_address(params) || Location.find(params[:id]) #find the location based on params[:location_id]
     unless current_user.check_near_locations(params) #check if the user has shared secret in the location and returns them
-      @locations = Location.close_locations_near_to_a_slug #find all locations near a params[:id]
+      @locations = Location.close_locations_near_to_a_slug(params) #find all locations near a params[:id]
+    end
+
+    respond_to do |f|
+      f.html
+      f.json {render json: @location, status: 201}
     end
   end
 
